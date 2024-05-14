61778730302a9cdfc7154eea0d9b458727fc18b6
Dave Bernhard <davidbernhard@live.com>
2024-05-14 18:30:13 +0100

    add express

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,26 @@
-console.log('Hello there!');
+import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+
+dotenv.config();
+const PORT = process.env.PORT;
+
+const app = express();
+// support parsing of application/json type post data
+app.use(bodyParser.json());
+
+app.get('/', (request: Request, response: Response) => {
+    response.status(200).send('Hello World');
+});
+
+app.post('/', (request: Request, response: Response) => {
+    const { body } = request;
+    console.log(body);
+    response.status(200).send('That was a post request');
+});
+
+app.listen(PORT, () => {
+    console.log('Server running at PORT: ', PORT);
+}).on('error', (error) => {
+    throw new Error(error.message);
+});
